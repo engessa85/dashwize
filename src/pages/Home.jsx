4d66316119ad7376b3a76795878c5eb3ee40a6c7
Mohamed Yehia <eng.essa85@gmail.com>
2023-08-24 12:27:46 +0300

    adding the second section of the landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,19 +17,54 @@ function Home() {
                 A small business is only as good as its tools
               </h1>
               <p className="para">
-                We’re different. Flex is the only saas business <br/> platform that
-                lets you run your business on one <br/> platform, seamlessly across
-                all digital channels.
+                We’re different. Flex is the only saas business <br /> platform
+                that lets you run your business on one <br /> platform,
+                seamlessly across all digital channels.
               </p>
               <div className="buttons">
-                <Link className="demobuttom" to="/">Request a Demo</Link>
-                <Link className="signupbutton" to="/signup">Sign Up</Link>
-
+                <Link className="demobuttom" to="/">
+                  Request a Demo
+                </Link>
+                <Link className="signupbutton" to="/signup">
+                  Sign Up
+                </Link>
               </div>
             </Col>
 
             <Col md={6} sm={12} className="first-section__right">
               <img src="assets/sectionOne.png" alt="error" />
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <section className="second-section">
+        <Container>
+          <Row>
+            <Col md={6} sm={12} className="second-section__left">
+              <h1 className="title">
+                Your life's work,
+                <br /> powered by our life's work
+              </h1>
+              <span className="line"></span>
+              <p className="para">
+                A unique and powerful software suite to transform the way you
+                work. Designed for businesses of all sizes, built by a company
+              </p>
+              <p className="para">
+                Run your entire business on Zoho with our unified cloud
+                software, designed to help you break down silos between
+                departments and increase organizational efficiency.
+              </p>
+              <div className="buttons">
+                <Link className="getstart" to="/">
+                  Get Started
+                </Link>
+              </div>
+            </Col>
+
+            <Col md={6} sm={12} className="second-section__right">
+              <img src="assets/sectionTwo.png" alt="error" />
             </Col>
           </Row>
         </Container>
