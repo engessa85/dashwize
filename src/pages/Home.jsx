eb7520ad6191f1953b5e0dd0beaacd3cccc6f937
Mohamed Yehia <eng.essa85@gmail.com>
2023-08-27 00:03:47 +0300

    adding the eighth section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -418,6 +418,57 @@ function Home() {
           </Row>
         </Container>
       </section>
+
+      <section className="eight-section">
+        <Container>
+          <Row className="eight-section__rowone">
+            <Col md={12} sm={12}>
+              <div className="features">NUMBERS</div>
+              <h1 className="title">We believe in the power of data</h1>
+              <p className="para">
+                Flex is the only business platform that lets you run your
+                business on one platform,
+                <br /> seamlessly across all digital channels.
+              </p>
+            </Col>
+          </Row>
+          <Row className="eight-section__rowtwo">
+            <Col className="col" md={3} sm={12}>
+              <div className="title">
+                <p>235.000</p>
+              </div>
+              <div className="lead">
+                <p>Projects completed</p>
+              </div>
+            </Col>
+            <Col className="col" md={3} sm={12}>
+              <div className="title">
+                <p>$10m</p>
+              </div>
+              <div className="lead">
+                <p>APR</p>
+              </div>
+            </Col>
+            <Col className="col" md={3} sm={12}>
+              <div className="title">
+                <p>+50.000</p>
+              </div>
+              <div className="lead">
+                <p>Hours Saved Annually</p>
+              </div>
+            </Col>
+            <Col className="col" md={3} sm={12}>
+              <div className="title">
+                <p>3.500</p>
+              </div>
+              <div className="lead">
+                <p>Unique Users</p>
+              </div>
+            </Col>
+            
+          </Row>
+        </Container>
+      </section>
     </div>
   );
 }
