ae7ad1422709ef2b6ff76201e1bc10116e1d2474
Mohamed Yehia <eng.essa85@gmail.com>
2023-08-27 13:04:18 +0300

    adding the footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -465,7 +465,41 @@ function Home() {
                 <p>Unique Users</p>
               </div>
             </Col>
-            
+          </Row>
+        </Container>
+      </section>
+
+      <section className="footer">
+        <Container>
+          <Row className="footer__rowone">
+            <img src="assets/logo_footer.png" alt="" />
+          </Row>
+          <Row className="footer__rowtwo">
+            <div className="col">
+              <Link>Product</Link>
+            </div>
+            <div className="col">
+              <Link>Features</Link>
+            </div>
+            <div className="col">
+              <Link>Pricing</Link>
+            </div>
+            <div className="col">
+              <Link>Resources</Link>
+            </div>
+            <div className="col">
+              <Link>Careers</Link>
+            </div>
+            <div className="col">
+              <Link>Help</Link>
+            </div>
+            <div className="col">
+              <Link>Privacy</Link>
+            </div>
+          </Row>
+          <span className="footer__line"></span>
+          <Row className="footer__rowthree">
+            <p>© 2023 dashwize. All rights reserved.</p>
           </Row>
         </Container>
       </section>
